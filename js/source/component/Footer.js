3de3be77e45aa884135cd332c3f4d925e6ddb841
KimNen <xogus7946@gmail.com>
2018-06-27 01:04:35 +0900

    Footer 제작

---
+++ js/source/component/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => (
+    <footer>
+        FooterPage
+    </footer>
+)
+export default Footer
--- js/source/shared/Root.js
+++ js/source/shared/Root.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../component/Header'
 import MainWindow from '../component/MainWindow'
+import Footer from '../component/Footer'
 
 const Root = () => (
     <div>
         <Header/>
         <MainWindow/>
+        <Footer/>
     </div>
 )
 
